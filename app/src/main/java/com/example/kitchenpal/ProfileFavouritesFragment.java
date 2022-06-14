38390682fe40ca9e3a3510e7a504b93f6db78464
yuxuanleong <elyse.xuan@gmail.com>
2022-06-14 14:29:15 +0800

    profile page ongoing

--- app/src/main/java/com/example/kitchenpal/ProfileFavouritesFragment.java
+++ app/src/main/java/com/example/kitchenpal/ProfileFavouritesFragment.java
@@ -17,21 +17,23 @@ import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link ProfileFavouritesFragment} factory method to
+ *
  * create an instance of this fragment.
  */
 public class ProfileFavouritesFragment extends Fragment {
-//
+
+//    // TODO: Rename parameter arguments, choose names that match
 //    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 //    private static final String ARG_PARAM1 = "param1";
 //    private static final String ARG_PARAM2 = "param2";
 //
+//    // TODO: Rename and change types of parameters
 //    private String mParam1;
 //    private String mParam2;
 //
-    public ProfileFavouritesFragment() {
-        // Required empty public constructor
-    }
+//    public ProfileFavouritesFragment() {
+//        // Required empty public constructor
+//    }
 //
 //    /**
 //     * Use this factory method to create a new instance of
@@ -39,10 +41,11 @@ public class ProfileFavouritesFragment extends Fragment {
 //     *
 //     * @param param1 Parameter 1.
 //     * @param param2 Parameter 2.
-//     * @return A new instance of fragment profileFavourites.
+//     * @return A new instance of fragment ProfileFavouritesFragment.
 //     */
-//    public static profileFavourites newInstance(String param1, String param2) {
-//        profileFavourites fragment = new profileFavourites();
+//    // TODO: Rename and change types and number of parameters
+//    public static ProfileFavouritesFragment newInstance(String param1, String param2) {
+//        ProfileFavouritesFragment fragment = new ProfileFavouritesFragment();
 //        Bundle args = new Bundle();
 //        args.putString(ARG_PARAM1, param1);
 //        args.putString(ARG_PARAM2, param2);
@@ -58,14 +61,14 @@ public class ProfileFavouritesFragment extends Fragment {
 //            mParam2 = getArguments().getString(ARG_PARAM2);
 //        }
 //    }
-        RecyclerView profileVerticalRec;
-        List<ProfileFavModel> profileVerNodeList;
-        ProfileFavAdapter profileFavAdapter;
+
+    RecyclerView profileVerticalRec;
+    List<ProfileFavModel> profileVerNodeList;
+    ProfileFavAdapter profileFavAdapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
         View root = inflater.inflate(R.layout.fragment_profile_favourites, container, false);
 
         profileVerticalRec = root.findViewById(R.id.profile_fav_rec);
--- app/src/main/java/com/example/kitchenpal/ProfileFragment.java
+++ app/src/main/java/com/example/kitchenpal/ProfileFragment.java
@@ -6,58 +6,53 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.fragment.app.Fragment;
+import androidx.viewpager2.widget.ViewPager2;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link ProfileFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import com.example.kitchenpal.adapters.VPA;
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
+
+///**
+// * A simple {@link Fragment} subclass.
+// * Use the {@link ProfileFragment#newInstance} factory method to
+// * create an instance of this fragment.
+// */
 public class ProfileFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    TabLayout tabLayout;
+    ViewPager2 viewpager;
 
     public ProfileFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment ProfileFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static ProfileFragment newInstance(String param1, String param2) {
-        ProfileFragment fragment = new ProfileFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
-
+//    @Override
+//    public void onCreate(Bundle savedInstanceState) {
+//        super.onCreate(savedInstanceState);
+//        if (getArguments() != null) {
+//            mParam1 = getArguments().getString(ARG_PARAM1);
+//            mParam2 = getArguments().getString(ARG_PARAM2);
+//        }
+//    }
+//
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+        addFragment(view);
+        return view;
+    }
+
+    private void addFragment(View view) {
+        tabLayout = view.findViewById(R.id.profileTabLayout);
+        viewpager = view.findViewById(R.id.viewPager);
+
+        VPA vpa = new VPA(getChildFragmentManager(),getLifecycle());
+        vpa.addFragments(new ProfileFavouritesFragment(), " Favourites");
+        vpa.addFragments(new profileMyRecipes(),"My Recipes");
+        viewpager.setAdapter(vpa);
+        new TabLayoutMediator(tabLayout, viewpager, (tab, pos) -> tab.setText(vpa.getTitles(pos))).attach();
     }
 }
+
